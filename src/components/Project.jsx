cf3db469c2f7c97aebe28c9af44fec87c354ed50
Jonathan Le <jonathanle062303@gmail.com>
2024-06-18 04:01:42 -0700

    Run Prettier

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,9 +4,16 @@ const Project = ({ name, image, website, github }) => {
   return (
     <div className="flex-none flex-col">
       <div className="flex justify-center items-center">
-        <Image className="rounded-t-2xl border-t border-x border-black" src={image} alt={name} style={{ width: "320px", height: "180px" }} />
+        <Image
+          className="rounded-t-2xl border-t border-x border-black"
+          src={image}
+          alt={name}
+          style={{ width: "320px", height: "180px" }}
+        />
       </div>
-      <div className="flex justify-center font-sans text-3xl bg-blue-300 text-white border border-black rounded-b-xl">{name}</div>
+      <div className="flex justify-center font-sans text-3xl bg-blue-300 text-white border border-black rounded-b-xl">
+        {name}
+      </div>
     </div>
   );
 };
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,11 +8,18 @@ const Projects = () => {
       <Title text="Projects" />
       <div className="flex gap-x-1 border flex-nowrap overflow-x-auto overflow-y-hidden">
         {projectData.map((project, index) => (
-          <Project key={index} className="" name={project.name} image={project.image} website={project.website} github={project.github} />
+          <Project
+            key={index}
+            className=""
+            name={project.name}
+            image={project.image}
+            website={project.website}
+            github={project.github}
+          />
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Projects;
